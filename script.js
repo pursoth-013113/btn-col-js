9ae8fd19353aa316e151feb627670b8cfcaab009
pursoth-013113 <122788660+pursoth-013113@users.noreply.github.com>
2024-11-14 15:14:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+/*click panna color change aggum */
+
+// const button=document.getElementById('btn')
+// const value=document.querySelector('.text')
+// let code=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+// const button1=document.getElementById('btn1')
+// const col=["red","blue","green"]
+// button.addEventListener('click',()=>{
+// let color='#'
+// for(i=0;i<6;i++){
+//     color+=code[random()]
+// }
+// document.body.style.backgroundColor=color
+// value.textContent=color
+// })
+// button1.addEventListener('click',()=>{
+    
+//     document.body.style.backgroundColor=col
+//     let random=Math.floor(Math.random()*col.length)
+//     document.body.style.backgroundColor=col[random]
+//     value.textContent=col[random]
+//     })
+// function random()
+// {
+//     return Math.floor(Math.random()*code.length)
+// }
+
+
+/*increment decrement  */
+// const button=document.querySelectorAll('.btn')
+// //console.log(button)//
+// const data=document.querySelector('.text')
+// let value=0
+// button.forEach((button)=>{
+
+// button.addEventListener('click',(e)=>{
+//     const styles=e.currentTarget.classList
+//     console.log(styles)
+//     if(styles.contains('increment')){
+//         value++
+//     }
+//     else if(styles.contains('decrement'))
+//     {
+//     value--
+// }
+//   else{ 
+//     value=0
+// }
+// data.textContent=value
+// })
+// })
+
+
+/*increment decrement easy method */
+
+// const button=document.querySelectorAll('.btn')
+// //console.log(button)//
+// const data=document.querySelector('.text')
+// let value=0
+// button.forEach((button)=>{
+
+// button.addEventListener('click',(e)=>{
+//     const styles=e.currentTarget.classList
+//     console.log(styles)
+//     if(styles.contains('increment')){
+//         value++
+//     }
+//     else if(styles.contains('decrement'))
+//     {
+//     value--
+// }
+//   else{ 
+//     value=0
+// }
+// data.textContent=value
+// })
+// })
